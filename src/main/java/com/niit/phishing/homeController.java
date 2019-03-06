8210898c24a192aedce09dada158f4f6b1f77409
VaibhavSingh98 <s.vaibhav1998@gmail.com>
2019-03-06 17:30:21 +0530

    First Connit

---
+++ src/main/java/com/niit/phishing/homeController.java
@@ -0,0 +1,16 @@
+package com.niit.phishing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	
+	@RequestMapping("/")
+	public String HomePage(Model m)
+	{
+		return "index";
+	}
+
+}
